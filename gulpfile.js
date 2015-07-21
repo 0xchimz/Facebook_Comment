0453da80f0b9a0783acce04e419779a17d9d64a2
ChinCluBi <chincub@gmail.com>
2015-07-21 16:42:32 +0700

    Parent process can create child process

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp')
+var spawn = require('child_process').spawn
+var node
+
+gulp.task('node', function (callback) {
+  if (node) {
+    console.log('Kill previous node!')
+    node.kill()
+  }
+  node = spawn('node', ['lib/app.js'], { stdio: 'inherit' })
+  node.on('close', function (code) {
+    if (code === 8) {
+      console.log('Error detected, waiting for changes...')
+    }
+  })
+})
+
+gulp.task('watch', function () {
+  gulp.watch('./lib/**/*.js', ['node'])
+})
+
+gulp.task('default', ['node', 'watch'])
+
+process.on('exit', function () {
+  if (node) {
+    console.log('Kill previous node!')
+    node.kill()
+  }
+})
